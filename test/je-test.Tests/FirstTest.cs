0c6b9c69883030e9188e253cd1ea6a7c2947899f
docleon <leon.hewitt@gmail.com>
2018-03-17 14:16:05 +0000

    add first test

---
+++ test/je-test.Tests/FirstTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace je_test.Tests
+{
+    public class FirstTest
+    {
+        [Test]
+        public void add_two_numbers()
+        {
+            Assert.That(2 + 2, Is.EqualTo(4));
+        }
+    }
+}
