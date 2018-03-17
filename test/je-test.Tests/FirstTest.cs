996a50cd44b1e563356c9e91474c47d56b59a59a
docleon <leon.hewitt@gmail.com>
2018-03-17 14:33:27 +0000

    get data back from je-api

--- test/je-test.Tests/FirstTest.cs
+++ test/je-test.Tests/FirstTest.cs
@@ -1,5 +1,10 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using NUnit.Framework;
 using NUnit.Framework.Constraints;
+using RestSharp;
 
 namespace je_test.Tests
 {
@@ -10,15 +15,21 @@ namespace je_test.Tests
         {
             var jeApiClient = new JeApiClient();
             var restaurants = jeApiClient.GetRestaurants();
-            Assert.That(restaurants, Is.Not.Null);
+            Assert.That(restaurants.Count, Is.GreaterThan(0));
         }
     }
 
     public class JeApiClient
     {
-        public object GetRestaurants()
+        public IList<object> GetRestaurants()
         {
-            return new { };
+            var client = new RestClient("http://public.je-apis.com/");
+            IRestRequest request = new RestRequest("restaurants?q=SE19");
+            request.AddHeader("Accept-Language", "en-GB");
+            request.AddHeader("Authorization", "Basic VGVjaFRlc3Q6bkQ2NGxXVnZreDVw");
+            var response = client.Get(request);
+            if (response.StatusCode != HttpStatusCode.OK) throw new Exception($"Error requesting data. Status: {response.StatusCode}, Content: {response.Content}");
+            return new List<object>() {new { }};
         }
     }
 }
